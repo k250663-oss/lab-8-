7b0bb3d0c49385e503c77bb0d2fdee48e336e567
Mohaddisa Zehra <k250663@nu.edu.pk>
2025-10-30 12:44:19 +0500

    8.2.c

--- 8.2.c
+++ 8.2.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    int sum1 =0  , sum2 = 0 ;
-    int year1[] = {12, 10, 15, 8, 5, 20, 25, 30, 10, 5, 8, 1};
-    int year2[] = {10, 12, 18, 9, 6, 22, 28, 35, 12, 7, 9, 16};
+    int year[2][12] = {{12, 10, 15, 8, 5, 20, 25, 30, 10, 5, 8, 1} , {10, 12, 18, 9, 6, 22, 28, 35, 12, 7, 9, 16}};
+   
     
-    for(int i=0 ; i<12 ; i++){
-        printf("year 1 ,month %d = %d photos\n" ,i+1 , year1[i]);
+    for(int i=0 ; i<2 ; i++){
+        for(int j=0 ; j<12 ; j++){
+          
+        printf("year %d ,month %d = %d photos\n" ,i+1 , j+1, year[i][j]);
     }
-    for(int i=0 ; i<12 ; i++){
-        printf("year 2 ,month %d = %d photos\n" ,i+1 , year2[i]);
     }
+
  
 
     return 0;
 }
+
